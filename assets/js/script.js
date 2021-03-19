8d71a4b1f090df792acac9fa1354abebecc5c6e2
Brian Adams <bpeteradams@gmail.com>
2021-03-18 21:40:06 -0400

    got discogs API to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ function getLyrics() {
         .then((res)=> res.json())
         .then((data)=>console.log(data))
 }
+
+function searchDiscogs() {
+    fetch(`https://api.discogs.com/database/search?q=Nirvana&type=artist&key=${discogsKey}&secret=${discogsSecret}`)
+        .then(res=> res.json())
+        .then(data=> console.log(data))
+    // fetch(url)
+    //     .then(function(res){
+    //         return res.json()
+    //     })
+    //     .then(function(data){
+    //         console.log(data)
+    //     })
+}
