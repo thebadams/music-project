f34b4ea16d54b4117101b8434d30ca25eaaa9caf
Brian Adams <bpeteradams@gmail.com>
2021-03-20 13:51:08 -0400

    wrote async functions to query discogs API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,52 +4,33 @@ function getLyrics() {
 		.then((data) => console.log(data));
 }
 
-function searchDiscogs() {
-	fetch(`https://api.discogs.com/database/search?q=Nirvana&type=artist&key=${discogsKey}&secret=${discogsSecret}`)
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data.results[0].resource_url);
-			return data.results[0].resource_url;
-		});
-	// fetch(url)
-	//     .then(function(res){
-	//         return res.json()
-	//     })
-	//     .then(function(data){
-	//         console.log(data)
-	//     })
+async function searchDiscogsArtistName() {
+	const response = await fetch(`https://api.discogs.com/database/search?q=Nirvana&type=artist&key=${discogsKey}&secret=${discogsSecret}`)
+	const data = await response.json()
+	return data.results[0].resource_url
 }
 
-async function searchDiscogs2() {
-    let searchBox = document.querySelector("input")
-    let searchQuery = searchBox.value
-    let requestURL = await fetch(`https://api.discogs.com/database/search?q=${searchQuery}&type=artist&key=${discogsKey}&secret=${discogsSecret}`)
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data.results[0].resource_url);
-			return data.results[0].resource_url;
-		});
-	fetch(requestURL)
-	    .then((res)=>{
-            console.log(res)
-            return res.json()})
-        .then((data)=> console.log(data))
+async function searchDiscogsArtistID() {
+    let requestURL = await searchDiscogsArtistName();
+    let response = await fetch(requestURL);
+    let data = await response.json();
+    return data.releases_url
 }
 
-function searchAudioDB() {
-    var xhr = new XMLHttpRequest()
-    xhr.open("GET", 'http://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay', true)
-    xhr.onreadystatechange = function () {
-  // In local files, status is 0 upon success in Mozilla Firefox
-  if(xhr.readyState === XMLHttpRequest.DONE) {
-    var status = xhr.status;
-    if (status === 0 || (status >= 200 && status < 400)) {
-      // The request has been completed successfully
-      console.log(xhr.responseText);
-    } else {
-      // Oh no! There has been an error with the request!
-    }
-  }
-};
-xhr.send()
+async function searchDiscogsArtistReleases() {
+    let requestURL = await searchDiscogsArtistID();
+    let response = await fetch(requestURL);
+    let data = await response.json()
+    return data.releases[1].resource_url
 }
+
+async function searchDiscogsReleaseTrackList() {
+    let requestURL = await searchDiscogsArtistReleases();
+    let response = await fetch(requestURL);
+    let data = await response.json();
+    return data.tracklist
+}
+
+//searchDiscogs3
+//get artist releases
+//filter released by role = "Main" or type = master
