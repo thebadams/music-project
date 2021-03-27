bd42a8935602787577de849297eeac0b330a90ff
Brian Adams <bpeteradams@gmail.com>
2021-03-27 09:20:35 -0400

    fixed saved-artists bug

--- assets/js/start-page.js
+++ assets/js/start-page.js
@@ -9,3 +9,32 @@ searchBtn.addEventListener("click", ()=>{
     let searchString = `./searchresults.html?q=${searchInputValue}`
     location.assign(searchString)
 })
+
+//saved artists
+let saveModalDismiss = document.querySelector(".save-dismiss")
+saveModalDismiss.addEventListener("click", ()=>{
+    let saveModal = document.querySelector(".save-modal");
+    saveModal.classList.remove("is-active");
+})
+
+let savedArtistsLink = document.querySelector("#saved-artists-link")
+savedArtistsLink.addEventListener("click", ()=>{
+    resetSavedArtistsModal();
+  let saveModal = document.querySelector(".save-modal");
+  saveModal.classList.add("is-active");
+   let saveOl = document.querySelector("#save-ol");
+  for(let i=0; i<savedArtists.length; i++ ) {
+    let saveListItem = document.createElement("li");
+    saveListItem.textContent=savedArtists[i]
+    saveOl.append(saveListItem)  
+  }  
+})
+
+function resetSavedArtistsModal() {
+    let saveOl = document.querySelector("#save-ol")
+    if(saveOl.childElementCount!==0) {
+        for(let i = saveOl.children.length-1; i>=0; i--){
+            saveOl.children[i].remove();
+        }
+    }
+}
